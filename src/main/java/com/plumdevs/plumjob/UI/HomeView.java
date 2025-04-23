c3092e1e4adfb8df902f9598dd12ecbd7daa8148
kingazm <kinga.zmuda@icloud.com>
2025-04-23 11:39:11 +0200

    Added sample home message

--- src/main/java/com/plumdevs/plumjob/UI/HomeView.java
+++ src/main/java/com/plumdevs/plumjob/UI/HomeView.java
@@ -1,9 +1,16 @@
 package com.plumdevs.plumjob.UI;
 
 import com.plumdevs.plumjob.UI.layout.MainLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route(value="home", layout = MainLayout.class)
 public class HomeView extends VerticalLayout {
+
+    HomeView() {
+        System.out.println("Home");
+        add(new H1("Welcome to Plum Job"));
+    }
 }
