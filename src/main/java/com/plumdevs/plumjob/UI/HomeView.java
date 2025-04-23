43ac34daf39c44d64c925370c7073644e5f224ce
kingazm <kinga.zmuda@icloud.com>
2025-04-23 11:54:59 +0200

    Added HomeView.java message

--- src/main/java/com/plumdevs/plumjob/UI/HomeView.java
+++ src/main/java/com/plumdevs/plumjob/UI/HomeView.java
@@ -3,6 +3,7 @@ package com.plumdevs.plumjob.UI;
 import com.plumdevs.plumjob.UI.layout.MainLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -12,5 +13,6 @@ public class HomeView extends VerticalLayout {
     HomeView() {
         System.out.println("Home");
         add(new H1("Welcome to Plum Job"));
+        add(new Paragraph("This is your homepage"));
     }
 }
