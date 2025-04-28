447937bb8d56f89334a1d2555ef9d3f0da3edf8c
Martyna Cios <ciosmartyna@gmail.com>
2025-04-28 13:31:28 +0200

    CvTemplatesController

--- src/main/java/com/plumdevs/plumjob/controller/CvTemplatesController.java
+++ src/main/java/com/plumdevs/plumjob/controller/CvTemplatesController.java
@@ -1,23 +1,32 @@
 package com.plumdevs.plumjob.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
 
 @Controller
 public class CvTemplatesController {
-    @GetMapping("template1")
-    public String GetTemplate1() {
-        return "template1.html";
+
+    @GetMapping("/CVBuilder/template1")
+    public ResponseEntity<Resource> serveTemplate1() throws IOException {
+        return ResponseEntity.ok()
+                .body(new ClassPathResource("/CvTemplates/template1.html"));
     }
 
-    @GetMapping("template2")
-    public String GetTemplate2() {
-        return "template2.html";
+    @GetMapping("/CVBuilder/template2")
+    public ResponseEntity<Resource> serveTemplate2() throws IOException {
+        return ResponseEntity.ok()
+                .body(new ClassPathResource("/CvTemplates/template2.html"));
     }
 
-    @GetMapping("template3")
-    public String GetTemplate3() { return "template3.html"; }
+    @GetMapping("/CVBuilder/template3")
+    public ResponseEntity<Resource> serveTemplate3() throws IOException {
+        return ResponseEntity.ok()
+                .body(new ClassPathResource("/CvTemplates/template3.html"));
+    }
 }
-
 
 
