dc4e41931331dd4601596c02142bf368418279f3
k. <66728641+kingazm@users.noreply.github.com>
2025-05-20 13:48:03 +0200

    Revert "Testing merge"

--- src/main/java/com/plumdevs/plumjob/UI/ArticlesView.java
+++ src/main/java/com/plumdevs/plumjob/UI/ArticlesView.java
@@ -23,58 +23,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import com.plumdevs.plumjob.UI.component.StickyAdBar;
-import com.plumdevs.plumjob.service.TagService;
-import com.vaadin.flow.spring.security.AuthenticationContext;
-
-@PermitAll
-@PageTitle("Plum Job - Articles")
-@Route(value="articles", layout = MainLayout.class)
-public class ArticlesView extends VerticalLayout {
-
-    private final ArticleService articleService = new ArticleService();
-
-    ArticlesView( TagService tagService, AuthenticationContext authContext ) throws IOException {
-        System.out.println("Articles/Tips");
-        add(new H2("Read articles by recruiters from top companies"));
-        setWidthFull();
-
-        add(articleService.createArticleThumbnail("interviews", "5 Quick Tips to Succeed in Tech Interviews"));
-        add(articleService.createArticleThumbnail("behavioural", "Acing the Behavioral Interview in Tech"));
-        add(articleService.createArticleThumbnail("resume", "Crafting a Standout Tech CV"));
-        add(articleService.createArticleThumbnail("jobhunt", "Mastering the Tech Job Hunt"));
-        add(articleService.createArticleThumbnail("portfolio", "Building a Portfolio That Gets Interviews"));
-
-        StickyAdBar adBar = new StickyAdBar(tagService, authContext);
-        add(adBar);
-    }
-}
-=======
-package com.plumdevs.plumjob.UI;
-
-import com.plumdevs.plumjob.UI.layout.MainLayout;
-import com.plumdevs.plumjob.service.ArticleService;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.card.Card;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import jakarta.annotation.security.PermitAll;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 @PermitAll
 @PageTitle("Plum Job - Articles")
 @Route(value="articles", layout = MainLayout.class)
