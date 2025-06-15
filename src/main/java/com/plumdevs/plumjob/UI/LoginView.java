05f872481f7f204d62e53de74bb259c8fd4dec87
kingazm <kinga.zmuda@icloud.com>
2025-06-15 21:02:56 +0200

    Forgot password? functionality added

--- src/main/java/com/plumdevs/plumjob/UI/LoginView.java
+++ src/main/java/com/plumdevs/plumjob/UI/LoginView.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
@@ -28,24 +30,31 @@ public class LoginView extends VerticalLayout {
 
 
         LoginForm login = new LoginForm();
-        //login.setId("login-form");
         login.setAction("login");
         login.setForgotPasswordButtonVisible(false);
         add(login);
-        //TODO: ERROR MESSAGE WHEN INCORRECT CREDENTIALS
 
         Button registerButton = new Button("Register");
         registerButton.addClassName("plum-text");
 
         RouterLink routerLink = new RouterLink("", RegisterView.class);
         routerLink.getElement().appendChild(registerButton.getElement());
-        add(routerLink);
 
         Button aboutButton = new Button("About");
         aboutButton.addClassName("plum-text-white");
 
         RouterLink aboutLink = new RouterLink("", AboutView.class);
         aboutLink.getElement().appendChild(aboutButton.getElement());
+
+        Button forgotPasswordButton = new Button("Forgot Password?");
+        forgotPasswordButton.addClassName("plum-text");
+        forgotPasswordButton.addClickListener(event -> {
+            Notification.show("Please contact support to reset your password at plumjob.contant@gmail.com", 3000, Notification.Position.MIDDLE);
+        });
+
+        HorizontalLayout bottomLinks = new HorizontalLayout(routerLink, forgotPasswordButton);
+        add(bottomLinks);
         add(aboutLink);
+
     }
 }
