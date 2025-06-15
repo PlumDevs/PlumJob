17262dac2feab79566d141752ca817a328947035
Tytus <tytus.szymczyk@gmail.com>
2025-06-16 00:11:34 +0200

    Update EventRepository.java
    Better event notification and also solved the problem with sending notifications between two days (it didn't send notifications when it's the next day, for example for an event at 00:30 it didn't send notifications at 23:30)

--- src/main/java/com/plumdevs/plumjob/repository/EventRepository.java
+++ src/main/java/com/plumdevs/plumjob/repository/EventRepository.java
@@ -5,16 +5,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
     @Query("SELECT e FROM Event e WHERE e.reminderSent = false AND e.eventDate = :date " +
             "AND e.eventTime BETWEEN :startTime AND :endTime")
     List<Event> findEventsToRemind(
             @Param("date") LocalDate date,
             @Param("startTime") LocalTime startTime,
             @Param("endTime") LocalTime endTime);
+
+    @Query("SELECT e FROM Event e WHERE e.reminderSent = false " +
+            "AND FUNCTION('CONCAT', e.eventDate, ' ', e.eventTime) BETWEEN :startDateTime AND :endDateTime")
+    List<Event> findEventsToRemindByDateTime(
+            @Param("startDateTime") LocalDateTime startDateTime,
+            @Param("endDateTime") LocalDateTime endDateTime);
+
     List<Event> findByEventDateAndEventTimeAndReminderSentFalse(LocalDate date, LocalTime time);
     List<Event> findByEventDateAndUsername(LocalDate date, String username);
     void deleteByIdAndUsername(Long id, String username);
