5d5a1398d88cf55395a726d41f0181c98e2a8f7a
k. <66728641+kingazm@users.noreply.github.com>
2025-06-15 20:24:12 +0200

    Update SecurityConfig.java

--- src/main/java/com/plumdevs/plumjob/config/SecurityConfig.java
+++ src/main/java/com/plumdevs/plumjob/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
@@ -21,6 +23,11 @@ import java.sql.ResultSet;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends VaadinWebSecurity {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
     protected void configure(HttpSecurity http) throws Exception {
